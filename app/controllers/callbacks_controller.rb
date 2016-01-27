2b9d026d2991c4ff1ac1f7ae5f9f25c70b1f346b
Julianne Gendrano <jujukoo@gmail.com>
2016-01-27 00:17:47 -0500

    removed pry

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,8 +1,6 @@
-require 'pry'
 class CallbacksController < Devise::OmniauthCallbacksController
   def slack
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    binding.pry
     sign_in_and_redirect @user
   end
 end
