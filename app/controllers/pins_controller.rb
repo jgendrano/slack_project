df6a0cd4ea3354314d0e95d331e66fd23cc637c6
Julianne Gendrano <jujukoo@gmail.com>
2016-01-30 14:57:09 -0500

    Revert "All the views are connected now, whew"

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,25 +4,18 @@ class PinsController < ApplicationController
   end
 
   def create
-    pin = params["data"]
-    @pin = Pin.create(user_id: current_user.id, slack_username: pin["user"],
-    ts: pin["ts"], slack_message: pin["text"])
-    if @pin.save
-      flash[:success] = "Book added successfully!"
-      redirect_to pins_path
-    else
-      flash[:warning] = @pin.errors.full_messages.join(', ')
-      redirect_to channel_path(channel.id)
-    end
+    @pin = Pin.new(params["data"]["user"], params["data"]["text"], params["data"]["ts"])
+    binding.pry
+    redirect_to pins_path
   end
+end
 
 private
 
-  def pin_params
-    params.require(:pin).permit(
-      :user,
-      :text,
-      :ts
-    )
-  end
+def pin_params
+  params.require(:pin).permit(
+    :user,
+    :text,
+    :ts
+  )
 end
